b4c911a73f8efedc497c1d868d1b37fe5833fe58
Wahid Shaikh <wshaikh@pureprofile.com>
2015-06-04 11:55:21 +0530

    removed javascript code from page and loaded from saperate js file
    removed javascript code from page and loaded from saperate js file

--- public/group.js
+++ public/group.js
@@ -1,8 +1,10 @@
 var gdApp = angular.module("gdApp", []);
 
 gdApp.service('groupRepository', function ($http) {
+
     this.getGroupDetails = function (groupuniquename) {
-        var url = "http://52.74.171.222/getgroupdetails/" + groupuniquename ;
+        //var url = "http://52.74.171.222/getgroupdetails/" + groupuniquename ;
+        var url = "http://salesforce-python.pureprofile.com/getgroupdetails/" + groupuniquename ;
         return $http.get(url);
     };
 });
@@ -10,8 +12,10 @@ gdApp.service('groupRepository', function ($http) {
 gdApp.controller('groupCtrl', function ($scope, groupRepository) {
     $scope.Groupuniquname = groupuniqued;
     $scope.groupdetails = {};
+    $scope.isdatafetching = true;
 
     groupRepository.getGroupDetails($scope.Groupuniquname).success(function (groupdata) {
         $scope.groupdetails = groupdata.group;
+        $scope.isdatafetching = false;
     });
 });
