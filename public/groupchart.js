335bf3a82b23c18b276164944df37cf75d00822b
Wahid Shaikh <wshaikh@pureprofile.com>
2015-06-26 15:00:44 +0530

    modified both the charts
    modified both the charts

--- public/groupchart.js
+++ public/groupchart.js
@@ -58,7 +58,7 @@ groupchartApp.controller('gcCtrl', function ($scope, metricgroupRepository) {
 
         var obj8={
             text : "NT",
-            percentage : parseFloat(groupmetricData.group_details.location.NT)
+            percentage : parseFloat(groupmetricData.group_details.location.nt)
         }
         locationdata.push(obj8);
 
@@ -100,11 +100,20 @@ groupchartApp.controller('gcCtrl', function ($scope, metricgroupRepository) {
         agergroups.push(obja6);
 
         var chartgroups = [];
-        chartgroups.push(locationdata);
-        chartgroups.push(agergroups);
+        var objLocGroup = {
+            isLocation : 1,
+            chartdata : locationdata
+        }
+        chartgroups.push(objLocGroup);
+
+        var objchartAgeGroup = {
+            isLocation : 0,
+            chartdata  : agergroups
+            }
+        chartgroups.push(objchartAgeGroup);
 
         var margin = { top: 40, right: 20, bottom: 30, left: 40 },
-        width = 960 - margin.left - margin.right,
+        width = 800 - margin.left - margin.right,
         height = 500 - margin.top - margin.bottom;
 
     var formatPercent = d3.format(".0%");
@@ -133,57 +142,173 @@ groupchartApp.controller('gcCtrl', function ($scope, metricgroupRepository) {
     var arrdata = [];
    // d3.tsv("/json/data.tsv", type, function (error, data) {
         for (i = 1; i <= chartgroups.length; i++) {
-            var objdata = chartgroups[i-1];
+         var objchartdata = chartgroups[i - 1];
+            var objdata = objchartdata.chartdata;
+            var isLocation = objchartdata.isLocation;
 
-            var ctrID = "chart" + i;
-            var svg = d3.select("#" + ctrID).append("svg")
-                .attr("width", width + margin.left + margin.right)
-                .attr("height", height + margin.top + margin.bottom)
-                .append("g")
-                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+            if (isLocation == 0) {
+                generateAgeChart(objdata);
+            } else {
+                var ctrID = "chart" + i;
+                var svg = d3.select("#" + ctrID).append("svg")
+                    .attr("width", width + margin.left + margin.right)
+                    .attr("height", height + margin.top + margin.bottom)
+                    .append("g")
+                    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-            svg.call(tip);
+               // svg.call(tip);
 
-            var maxPercentage = d3.max(objdata, function (d) { return d.percentage;});
-            // d3.tsv("frequency.tsv", type, function (error, data) {
-            x.domain(objdata.map(function (d) { return d.text; }));
-            //y.domain([0, d3.max(objdata, function (d) { return d.percentage;})]);
-            //y.domain([0, maxPercentage+5]);
-            y.domain([0, 100]);
+                var maxPercentage = d3.max(objdata, function (d) {
+                    return d.percentage;
+                });
+                // d3.tsv("frequency.tsv", type, function (error, data) {
+                x.domain(objdata.map(function (d) {
+                    return d.text;
+                }));
+                //y.domain([0, d3.max(objdata, function (d) { return d.percentage;})]);
+                //y.domain([0, maxPercentage+5]);
+                y.domain([0, 100]);
 
-            svg.append("g")
-                .attr("class", "x axis")
-                .attr("transform", "translate(0," + height + ")")
-                .call(xAxis);
-            //.append("text")
-            //.attr("transform", "rotate(360)")
-            //.style("text-anchor", "end")
-            //.text("Country");
+                svg.append("g")
+                    .attr("class", "x axis")
+                    .attr("transform", "translate(0," + height + ")")
+                    .call(xAxis);
+                //.append("text")
+                //.attr("transform", "rotate(360)")
+                //.style("text-anchor", "end")
+                //.text("Country");
 
+                svg.append("g")
+                    .attr("class", "y axis")
+                    .call(yAxis)
+                    .append("text")
+                    .attr("transform", "rotate(-90)")
+                    .attr("y", 6)
+                    .attr("dy", ".71em")
+                    .style("text-anchor", "end")
+                    //.text("Percentage");
 
-            svg.append("g")
-                .attr("class", "y axis")
-                .call(yAxis)
-                .append("text")
-                .attr("transform", "rotate(-90)")
-                .attr("y", 6)
-                .attr("dy", ".71em")
-                .style("text-anchor", "end")
-                .text("Percentage");
+                svg.selectAll(".bar")
+                    .data(objdata)
+                    .enter().append("rect")
+                    .attr("class", "bar")
+                    .attr("x", function (d) {
+                        return x(d.text);
+                    })
+                    .attr("width", x.rangeBand())
+                    .attr("y", function (d) {
+                        return y(d.percentage);
+                    })
+                    .attr("height", function (d) {
+                        return height - y(d.percentage);
+                    })
+                    .on('mouseover', tip.show)
+                    .on('mouseout', tip.hide)
 
-            svg.selectAll(".bar")
-                .data(objdata)
-                .enter().append("rect")
-                .attr("class", "bar")
-                .attr("x", function (d) { return x(d.text); })
-                .attr("width", x.rangeBand())
-                .attr("y", function (d) { return y(d.percentage); })
-                .attr("height", function (d) { return height - y(d.percentage); })
-                .on('mouseover', tip.show)
-                .on('mouseout', tip.hide)
+                svg.selectAll("text.bar")
+                    .data(objdata)
+                    .enter().append("text")
+                    //.attr("class", "value bartext")
+                    .attr("class","bartext")
+                    .attr("text-anchor", "middle")
+                    .attr("dx", "1.5em") //vertical align middle
+                    .attr("dy", ".70em") //vertical align middle
+                    .attr("x", function(d) { return x(d.text); })
+                    .attr("y", function(d) { return y(d.percentage) - 12; })
+                    .text(function(d) { return d.percentage; });
+            }
         }
     });
   //  });
+
+    function generateAgeChart(data){
+
+        var tip = d3.tip()
+            .attr('class', 'd3-tip')
+            .offset([-10, 0])
+            .html(function (d) {
+                return "<strong>Percentage:</strong> <span style='color:red'>" + d.percentage + "</span>";
+            })
+
+        var chart = document.getElementById("chart2"),
+            axisMargin = 20,
+            margin = 20,
+            valueMargin = 4,
+            width = chart.offsetWidth,
+            height = chart.offsetHeight,
+            barHeight = (height - axisMargin - margin * 2) * 0.4 / data.length,
+            barPadding = (height - axisMargin - margin * 2) * 0.6 / data.length,
+            data, bar, svg, scale, xAxis, labelWidth = 0;
+
+        max = d3.max(data.map(function (i) {
+            return i.percentage;
+        }));
+
+        svg = d3.select(chart)
+            .append("svg")
+            .attr("width", width)
+            .attr("height", 400);
+
+        //svg.call(tip);
+
+        bar = svg.selectAll("g")
+            .data(data)
+            .enter()
+            .append("g");
+
+        bar.attr("class", "bar")
+            .attr("cx", 0)
+            .attr("transform", function (d, i) {
+                return "translate(" + margin + "," + (i * (barHeight + barPadding) + barPadding) + ")";
+            });
+
+        bar.append("text")
+           // .attr("class", "tick value")
+            .attr("class","bartext")
+            .attr("y", barHeight / 2)
+            .attr("dy", ".35em") //vertical align middle
+            .text(function (d) {
+                return d.text;
+            }).each(function () {
+                labelWidth = Math.ceil(Math.max(labelWidth, this.getBBox().width));
+            });
+
+        scale = d3.scale.linear()
+            .domain([0, max + 10])
+            .range([0, width - margin * 2 - labelWidth]);
+
+        xAxis = d3.svg.axis()
+            .scale(scale)
+            .tickSize(-height + 2 * margin + axisMargin)
+            .orient("bottom");
+
+        bar.append("rect")
+            .attr("transform", "translate(" + labelWidth + ", 0)")
+            .attr("height", barHeight)
+            .attr("width", function (d) {
+                return scale(d.percentage);
+            });
+
+        bar.append("text")
+           // .attr("class", "value")
+            .attr("class","bartext")
+            .attr("y", barHeight / 2)
+            .attr("text-anchor", "middle")
+            .attr("dx", "5.6em") //margin right
+            .attr("dy", ".35em") //vertical align middle
+            .attr("text-anchor", "end")
+            .text(function (d) {
+                return d.percentage;
+            })
+            .attr("x", function (d) {
+                var width = this.getBBox().width;
+                return Math.max(width + valueMargin, scale(d.percentage));
+            });
+              svg.insert("g", ":first-child")
+            .attr("class", "axis")
+            .attr("transform", "translate(" + (margin + labelWidth) + "," + (height - axisMargin - margin) + ")")
+            .call(xAxis);
+    }
 
     function type(d) {
         d.percentage = +d.percentage;
