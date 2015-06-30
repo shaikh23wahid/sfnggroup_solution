1838df0bda689493210dea1b95e74ed4a067d4fa
Wahid Shaikh <wshaikh@pureprofile.com>
2015-06-30 14:17:04 +0530

    hardcoaded breadcrumbs added
    hardcoaded breadcrumbs added

--- public/categorydetails.js
+++ public/categorydetails.js
@@ -12,6 +12,29 @@ categoryApp.controller('categoryCtrl', function ($scope, categorydetailsReposito
     $scope.categorydetails = {};
     $scope.isdatafetching = true;
 
+    $scope.breadcrumbs = [
+        {
+            catname : "About You",
+            uname:"about-you"
+        },
+        {
+            catname : "Pets",
+            uname:"pets"
+        },
+        {
+            catname : "Pet Insurance",
+            uname:"pet-insurance"
+        },
+        {
+            catname : "Pet Insurance2",
+            uname:"pet-insurance2"
+        },
+        {
+            catname : "Pet Insurance3",
+            uname:"pet-insurance3"
+        }
+    ];
+
     categorydetailsRepository.getCategoryDetails($scope.categoryuniquename).success(function (categoryData) {
         $scope.categorydetails = categoryData.category_details;
         $scope.isdatafetching = false;
